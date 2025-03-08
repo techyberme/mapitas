a437e2ad62880ba87d487d5bdb2ad7ddb95bce77
techyberme <107142731+techyberme@users.noreply.github.com>
2025-03-08 12:48:09 +0100

    Add files via upload

---
+++ rawmap.py
@@ -0,0 +1,66 @@
+from polyline import decode
+import folium
+import requests
+def actualizar(refresh_token :str):
+    try:
+        # Actualizo el token
+        response = requests.post(f"https://www.strava.com/oauth/token?client_id=143763&client_secret=9ddc6c13807019d306436f8f13972b2936a26e47&refresh_token={refresh_token}&grant_type=refresh_token")
+        refresh= response.json()
+        token=refresh["access_token"]
+        print(token)
+                        # Check if the request was successful
+        response.raise_for_status()  # Raises an error for 4xx/5xx HTTP status codes
+    except requests.exceptions.RequestException as e:
+                print(f"Error: {e}")  # Print the error if there is an issue with the request 
+    # Try making the GET request
+    # The URL for the Strava API request
+    url = f"https://www.strava.com/api/v3/athlete/activities?access_token={token}&per_page=200&page=1"
+    try:
+        # Send the request with the authorization header
+        stats = requests.get(url).json()
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")  # Print the error if there is an issue with the request
+
+    l= folium.Map(location=[42.794776, -1.659370], zoom_start=5)
+    for i in range(len(stats)):
+        poly = stats[i]["map"]["summary_polyline"]
+        deporte=stats[i]["sport_type"]
+        distancia= round(stats[i]["distance"]/1000,2)
+        nombre=stats[i]["name"]
+        if deporte == "Walk":
+            gradiente= 255-int(distancia*255/40)
+            colores= f"rgb({gradiente}, 0, 0)"
+        elif deporte== "Run":
+            gradiente=255 - int(distancia*255/20)
+
+            colores= f"rgb(0, {gradiente}, 0)"
+        else:
+            gradiente= 255-int(distancia*255/150)
+            colores= f"rgb(0, 0, {gradiente})"
+        try:
+            vel=round(60/(stats[i]["average_speed"]*3.6),2)
+        except ZeroDivisionError:
+            vel="-"
+        if poly != "":
+            decoded_coordinates = decode(poly)
+            listica= [list(coord) for coord in decoded_coordinates]
+            folium.PolyLine(
+            listica,
+            color=colores,
+            tooltip=f"Nombre: {nombre} \n , Distancia: {distancia} km \n , Velocidad = {vel} min/km"
+            ).add_to(l)
+    
+    
+    map_title = "Las rutas"
+    title_html = f'<h1 style="position:absolute;z-index:100000;left:40vw" >{map_title}</h1>'
+    l.get_root().html.add_child(folium.Element(title_html))
+    l.save("./templates/stravastreamlit.html")
+
+
+
+
+
+
+
+#actualizar("0c4943d0df64282d2eee68a5966e5f7fdf1e67d5")
---
+++ web.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from rawmap import actualizar
+import requests
+url = "https://www.strava.com/oauth/authorize?client_id=143763&redirect_uri=http://localhost:8502&response_type=code&scope=activity:read_all"
+st.write("check out this [link](%s)" % url)
+
+    # Step 3: Retrieve the authorization code from the redirected URL
+query_params = st.query_params  # Capture URL parameters
+auth_code = query_params.get("code")
+
+if "code" in query_params and auth_code is None:
+        st.rerun()  # Force a rerun to capture query parameters
+
+if auth_code:
+        st.success(f"Authorization Code: {auth_code}")
+        codigo=1
+else:
+        st.warning("No authorization code found. Please authorize the app.")
+
+
+button=st.button("Pulsa para la magia")
+if button:
+    url_refresh=f"https://www.strava.com/oauth/token?client_id=143763&client_secret=9ddc6c13807019d306436f8f13972b2936a26e47&code={auth_code}&grant_type=authorization_code"
+    try:     # Sending POST request with data as JSON
+            response = requests.post(url_refresh)
+            # Check if the request was successful
+            #response.raise_for_status()  # Raises an error for 4xx/5xx HTTP status codes
+            response=response.json()
+            nombre_atleta=response["athlete"]["firstname"]
+            id_atleta= response["athlete"]["id"]
+            refresh_token=response["refresh_token"]
+            actualizar(refresh_token)
+    except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+path_to_html = "./templates/stravastreamlit.html"
+
+# Read file and keep in variable
+with open(path_to_html,'r') as f: 
+    html_data = f.read()
+
+## Show in webpage
+st.components.v1.html(html_data,height=500)
