3ee5978ee2de4a667ae63bed08cce6ab74550d25
techyberme <107142731+techyberme@users.noreply.github.com>
2025-03-09 10:37:13 +0100

    Update web.py

--- web.py
+++ web.py
@@ -36,7 +36,8 @@ try:
         path_to_html = "./templates/stravastreamlit.html"
 
         # Read file and keep in variable
-        with open(path_to_html,'r') as f: 
+        temp_path = "/tmp/stravastreamlit.html"
+        with open(temp_path,'r') as f: 
                 html_data = f.read()
 
         ## Show in webpage
