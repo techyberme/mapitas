575c2ac8c7034d88be3eef0286275b07133fb084
techyberme <107142731+techyberme@users.noreply.github.com>
2025-03-09 13:00:40 +0100

    minorchange

--- web.py
+++ web.py
@@ -20,32 +20,34 @@ if "code" in query_params and st.session_state["auth_code"]:
 else:
         st.write("🔗 Consigue los datos aqui: [link](%s)" % url)
 
+if "code" in query_params and st.session_state["auth_code"]:
+        st.session_state["authenticated"] = True  # Mark user as authenticated
+        st.write("✅ Ya estás registrado, ahora puedes ver tu mapa!")
+        st.session_state["button"]=st.button("Pulsa para la magia")
+        if st.session_state["button"]:  
+                url_refresh=f"https://www.strava.com/oauth/token?client_id=143763&client_secret=9ddc6c13807019d306436f8f13972b2936a26e47&code={st.session_state["auth_code"]}&grant_type=authorization_code"
+        try:     # Sending POST request with data as JSON
+                response = requests.post(url_refresh)
+                # Check if the request was successful
+                #response.raise_for_status()  # Raises an error for 4xx/5xx HTTP status codes
+                response=response.json()
+                nombre_atleta=response["athlete"]["firstname"]
+                id_atleta= response["athlete"]["id"]
+                refresh_token=response["refresh_token"]
+                actualizar(refresh_token)
+        except Exception as e:
+                st.error(f"An error occurred: {e}")
 
-st.session_state["button"]=st.button("Pulsa para la magia")
-if st.session_state["button"]:  
-    url_refresh=f"https://www.strava.com/oauth/token?client_id=143763&client_secret=9ddc6c13807019d306436f8f13972b2936a26e47&code={st.session_state["auth_code"]}&grant_type=authorization_code"
-    try:     # Sending POST request with data as JSON
-            response = requests.post(url_refresh)
-            # Check if the request was successful
-            #response.raise_for_status()  # Raises an error for 4xx/5xx HTTP status codes
-            response=response.json()
-            nombre_atleta=response["athlete"]["firstname"]
-            id_atleta= response["athlete"]["id"]
-            refresh_token=response["refresh_token"]
-            actualizar(refresh_token)
-    except Exception as e:
-            st.error(f"An error occurred: {e}")
+        if st.session_state["button"]:            
+                try:
+                        path_to_html = "./templates/stravastreamlit.html"
 
-if st.session_state["button"]:            
-        try:
-                path_to_html = "./templates/stravastreamlit.html"
+                        # Read file and keep in variable
+                        temp_path = f"/tmp/stravastreamlit.html"
+                        with open(temp_path,'r') as f: 
+                                html_data = f.read()
 
-                # Read file and keep in variable
-                temp_path = f"/tmp/stravastreamlit.html"
-                with open(temp_path,'r') as f: 
-                        html_data = f.read()
-
-                ## Show in webpage
-                st.components.v1.html(html_data,height=500)
-        except Exception:
-                None
+                        ## Show in webpage
+                        st.components.v1.html(html_data,height=500)
+                except Exception:
+                        None
