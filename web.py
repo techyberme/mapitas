78920369e9553a73e947cc79cb7b8da6fab02f8b
techyberme <107142731+techyberme@users.noreply.github.com>
2025-03-16 15:35:50 +0100

    h

--- web.py
+++ web.py
@@ -13,10 +13,6 @@ st.session_state["auth_code"] = query_params.get("code")
 
 if "code" in query_params and st.session_state["auth_code"] is None:
         st.rerun()  # Force a rerun to capture query parameters
-if "fig" not in st.session_state:  
-    st.session_state["fig"] = None
-if "map_html" not in st.session_state:    
-    st.session_state["map_html"] = None
 
 if "code" in query_params and st.session_state["auth_code"]:
         st.session_state["authenticated"] = True  # Mark user as authenticated
@@ -27,7 +23,8 @@ else:
 
 if "code" in query_params and st.session_state["auth_code"]:
         st.session_state["authenticated"] = True  # Mark user as authenticated
-        if st.session_state["map_html"] is None: 
+        st.session_state["button"]=st.button("Pulsa para la magia")
+        if st.session_state["button"]:  
                 url_refresh=f"https://www.strava.com/oauth/token?client_id=143763&client_secret=9ddc6c13807019d306436f8f13972b2936a26e47&code={st.session_state["auth_code"]}&grant_type=authorization_code"
                 try:     # Sending POST request with data as JSON
                         response = requests.post(url_refresh)
@@ -38,16 +35,26 @@ if "code" in query_params and st.session_state["auth_code"]:
                         id_atleta= response["athlete"]["id"]
                         refresh_token=response["refresh_token"]
                         st.session_state["fig"]=actualizar(refresh_token)
-                        with open("./templates/stravastreamlit.html", "r") as f:
-                                st.session_state["map_html"] = f.read()
                         
                 except Exception as e:
                         #st.error(f"An error occurred: {e}")
                         None
         st.session_state["graf"] = st.radio(" ",['Mapa', 'kms Acumulados']) 
-        if st.session_state["graf"] == "Mapa" and st.session_state["map_html"]:  
-                st.components.v1.html(st.session_state["map_html"], height=500)
-        if st.session_state["graf"]=="kms Acumulados" and st.session_state["fig"] is not None: 
+        if st.session_state["graf"]=="Mapa" and st.session_state["button"]:            
+                try:
+                        path_to_html = "./templates/stravastreamlit.html"
+
+                        # Read file and keep in variable
+                        temp_path = f"/tmp/stravastreamlit.html"
+                        with open(temp_path,'r') as f: 
+                                html_data = f.read()
+
+                        ## Show in webpage
+                        st.components.v1.html(html_data,height=500)
+                except Exception:
+                        None
+        if st.session_state["graf"]=="kms Acumulados" and st.session_state["button"]: 
+                if st.session_state["fig"]:
                         st.pyplot(st.session_state["fig"])  # Use st.plotly_chart(fig) if it's Plotly
-        elif st.session_state["graf"] == "kms Acumulados":
-                        st.warning("No figure available to display.")
+                else:
+                        st.warning("No figure available to display.") 
